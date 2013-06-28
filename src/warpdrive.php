2229a281b17293d0bd2643e5c98cbdf5ef75dd6e
Ferdi van der Werf <efcm@vdwerf.eu>
2013-06-28 09:37:11 +0200

    Start on warpdrive plugin

---
+++ src/warpdrive.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin name: Warpdrive
+ * Plugin URI: https://github.com/Savvii/warpdrive
+ * Description: Hosting plugin for Savvii
+ * Version: 0.1
+ * Author: Ferdi van der Werf <ferdi@savvii.nl>
+ * Author URI:
+ * License: All rights remain with Savvii
+ */
+
