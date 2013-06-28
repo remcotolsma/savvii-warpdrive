c10d12a829d8ba453165d33eb607e7be4cb4536b
Ferdi van der Werf <efcm@vdwerf.eu>
2013-06-28 09:36:58 +0200

    Added rsync for linode
    You need to be in nginx group to use this script.

---
+++ rsyncscript.sh
@@ -0,0 +1,14 @@
+# To use you need to be in the nginx group on linode
+if [ $# -eq 1 ]
+then
+	while true
+	do
+		# rsync - recursive, skip on checksum, human readable, compress, verbose
+		rsync -rchzv src/ linode:/usr/share/nginx/html/wp-content/plugins/warpdrive/
+		# Sleep 1s
+		sleep 2
+	done
+else
+	# rsync - once, skip on checksum, human readable, compress, verbose
+	rsync -rchzv src/ linode:/usr/share/nginx/html/wp-content/plugins/warpdrive/
+fi
