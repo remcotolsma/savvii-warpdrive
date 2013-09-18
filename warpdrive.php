c2180c1b7b2673bc2bc5761cfcaaed219d7ce7bb
Ferdi van der Werf <efcm@vdwerf.eu>
2013-09-18 23:41:18 +0200

    Updated plugin version
    Fixes #21

--- warpdrive.php
+++ warpdrive.php
@@ -3,7 +3,7 @@
  * Plugin name: Warpdrive
  * Plugin URI: https://github.com/Savvii/warpdrive
  * Description: Hosting plugin for Savvii
- * Version: 0.1
+ * Version: 0.10.1
  * Author: Ferdi van der Werf <ferdi@savvii.nl>
  * Author URI:
  * License: All rights remain with Savvii
